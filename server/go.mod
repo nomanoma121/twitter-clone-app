d310b63d7f19b699d64ecee9780c88504d9e4e53
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-01-02 15:29:15 +0900

    chore: add mysql

--- server/go.mod
+++ server/go.mod
@@ -2,9 +2,16 @@ module server
 
 go 1.23.3
 
-require github.com/labstack/echo/v4 v4.13.3
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/labstack/echo/v4 v4.13.3
+)
+
+require golang.org/x/time v0.8.0 // indirect
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- server/main.go
+++ server/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const (
@@ -11,7 +18,50 @@ const (
 )
 
 func main() {
+	user := os.Getenv("MYSQL_USER")
+	password := os.Getenv("MYSQL_PASSWORD")
+	dbname := os.Getenv("MYSQL_DATABASE")
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+
+	if user == "" || password == "" || dbname == "" || host == "" || port == "" {
+		log.Fatal("環境変数が設定されていません")
+	}
+
+	mode := os.Getenv("MODE")
+	if mode == "" {
+		mode = "development"
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, dbname)
+
+	// DB接続、5秒ごとに10回リトライ (MySQLコンテナが立ち上がるまで待つ)
+	db, err := sqlx.Connect("mysql", dsn)
+	if err != nil {
+		log.Printf("DB接続に失敗しました: %v", err)
+		for i := 0; i < 10; i++ {
+			err = db.Ping()
+			if err == nil {
+				break
+			}
+			log.Printf("DB接続に失敗しました (%d): %v", i+1, err)
+			time.Sleep(5 * time.Second)
+		}
+	}
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	e := echo.New()
+
+	if mode == "development" {
+		e.Debug = true
+		e.Use(middleware.Logger())
+		e.Use(middleware.Recover())
+	}
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(200, "Hello, World!")
 	})
