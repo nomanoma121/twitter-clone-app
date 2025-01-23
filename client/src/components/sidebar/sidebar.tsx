5c708507c29c36419bd150397ce71ce12908af2a
kou <107801494+nomanoma121@users.noreply.github.com>
2025-01-23 17:08:09 +0900

    サイドバーからツイートできるようにした

--- client/src/components/sidebar/sidebar.tsx
+++ client/src/components/sidebar/sidebar.tsx
@@ -6,7 +6,12 @@ import { PiBell } from "react-icons/pi";
 import { LuMail } from "react-icons/lu";
 import { CiCircleMore } from "react-icons/ci";
 import { GiFeather } from "react-icons/gi";
+import { useNavigate } from "react-router";
+import { useLocation } from "react-router";
+
 export const Sidebar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   return (
     <div className="Sidebar">
       <div className="Sidebar__icon__container">
@@ -30,7 +35,17 @@ export const Sidebar = () => {
       <div className="Sidebar__icon__container">
         <CiCircleMore className="Sidebar__icon" />
       </div>
-      <div className="Tweet__icon__container">
+      <div
+        className="Tweet__icon__container"
+        onClick={() =>
+          navigate("/compose/tweet", {
+            state: {
+              background: location,
+              tweet: { type: "tweet" },
+            },
+          })
+        }
+      >
         <GiFeather className="Tweet__icon" />
       </div>
     </div>
