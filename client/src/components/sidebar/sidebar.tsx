933f1ba435acbd3026082754e30247baed136559
kou <107801494+nomanoma121@users.noreply.github.com>
2025-01-23 17:01:35 +0900

    サイドバーにツイートアイコンを追加

--- client/src/components/sidebar/sidebar.tsx
+++ client/src/components/sidebar/sidebar.tsx
@@ -3,33 +3,36 @@ import { FaRegUser, FaTwitter } from "react-icons/fa";
 import { GoHomeFill } from "react-icons/go";
 import { IoSearchOutline } from "react-icons/io5";
 import { PiBell } from "react-icons/pi";
-import { LuMail } from "react-icons/lu"; 
+import { LuMail } from "react-icons/lu";
 import { CiCircleMore } from "react-icons/ci";
-
+import { GiFeather } from "react-icons/gi";
 export const Sidebar = () => {
   return (
     <div className="Sidebar">
-      <div className="Sidebar__icon">
-        <FaTwitter />
+      <div className="Sidebar__icon__container">
+        <FaTwitter className="Sidebar__icon" />
       </div>
-      <div className="Sidebar__icon">
-      <GoHomeFill />
+      <div className="Sidebar__icon__container">
+        <GoHomeFill className="Sidebar__icon" />
       </div>
-      <div className="Sidebar__icon">
-        <IoSearchOutline />
+      <div className="Sidebar__icon__container">
+        <IoSearchOutline className="Sidebar__icon" />
       </div>
-      <div className="Sidebar__icon">
-        <PiBell />
+      <div className="Sidebar__icon__container">
+        <PiBell className="Sidebar__icon" />
       </div>
-      <div className="Sidebar__icon">
-        <LuMail />
+      <div className="Sidebar__icon__container">
+        <LuMail className="Sidebar__icon" />
       </div>
-      <div className="Sidebar__icon">
-        <FaRegUser />
+      <div className="Sidebar__icon__container">
+        <FaRegUser className="Sidebar__icon" />
       </div>
-      <div className="Sidebar__icon">
-        <CiCircleMore />
+      <div className="Sidebar__icon__container">
+        <CiCircleMore className="Sidebar__icon" />
+      </div>
+      <div className="Tweet__icon__container">
+        <GiFeather className="Tweet__icon" />
       </div>
     </div>
-  )
-}
+  );
+};
