320d4f7b5f057525866697be657b3b73d450001c
kou <107801494+nomanoma121@users.noreply.github.com>
2025-01-24 15:46:05 +0900

    feat: api/likeを実装

--- server/handler/like.go
+++ server/handler/like.go
@@ -18,99 +18,34 @@ func NewLikeHandler(db *sqlx.DB) *LikeHandler {
 }
 
 func (h *LikeHandler) Register(g *echo.Group) {
-	g.POST("/like", h.Like)
-	g.DELETE("/like", h.Like)
-	// g.GET("/like/:id", h.GetLike)
-	// g.GET("/likes", h.GetLikes)
+	g.POST("/like/:id", h.CreateLike)
+	g.DELETE("/like/:id", h.DeleteLike)
 }
 
-type LikeRequest struct {
-	Liked bool `json:"liked" validate:"required"`
-}
-
-type LikeResponse struct {
-	Liked bool `json:"liked"`
-}
-
-func (h *LikeHandler) Like(c echo.Context) error {
-	userID := c.Get("user_id").(int)
-
-	var req LikeRequest
-	if err := c.Bind(&req); err != nil {
-		return c.JSON(400, map[string]string{"message": "Invalid request body"})
-	}
-
-	if err := h.validator.Struct(req); err != nil {
-		return c.JSON(400, map[string]string{"message": "Invalid request body"})
-	}
-
-	tweetID := c.Param("id")
-
-	if req.Liked {
-		_, err := h.db.Exec("INSERT INTO likes (tweet_id, user_id) VALUES (?, ?)", tweetID, userID)
-		if err != nil {
-			log.Println(err)
-			return c.JSON(500, map[string]string{"message": "Internal Server Error"})
-		}
-	} else {
-		_, err := h.db.Exec("DELETE FROM likes WHERE tweet_id = ? AND user_id = ?", tweetID, userID)
-		if err != nil {
-			log.Println(err)
-			return c.JSON(500, map[string]string{"message": "Internal Server Error"})
-		}
-	}
-
-	// 現在の状態を返す
-	return c.JSON(200, GetLikeResponse{Liked: req.Liked})
-}
-
-type GetLikeResponse struct {
-	Liked bool `json:"liked"`
-}
-
-func (h *LikeHandler) GetLike(c echo.Context) error {
+func (h *LikeHandler) CreateLike(c echo.Context) error {
 	userID := c.Get("user_id").(int)
 	tweetID := c.Param("id")
 
-	var count int
-	err := h.db.Get(&count, "SELECT COUNT(*) FROM likes WHERE tweet_id = ? AND user_id = ?", tweetID, userID)
+	log.Printf("userID: %d, tweetID: %s", userID, tweetID)
+
+	_, err := h.db.Exec("INSERT INTO likes (user_id, tweet_id) VALUES (?, ?)", userID, tweetID)
 	if err != nil {
 		log.Println(err)
-		return c.JSON(500, map[string]string{"message": "Internal Server Error"})
+		return c.NoContent(500)
 	}
 
-	return c.JSON(200, GetLikeResponse{Liked: count != 0})
+	return c.NoContent(200)
 }
 
-// type GetLikesResponse struct {
-// 	ID    int `json:"id"`
-// 	Tweet struct {
-// 		ID   int `json:"id"`
-// 		User struct {
-// 			ID        int    `json:"id"`
-// 			DisplayID string `json:"display_id"`
-// 			Name      string `json:"name"`
-// 		} `json:"user"`
-// 		Content *string `json:"content"`
-// 	} `json:"tweet"`
-// }
+func (h *LikeHandler) DeleteLike(c echo.Context) error {
+	userID := c.Get("user_id").(int)
+	tweetID := c.Param("id")
 
-// func (h *LikeHandler) GetLikes(c echo.Context) error {
-// 	userID := c.Get("user_id").(int)
+	_, err := h.db.Exec("DELETE FROM likes WHERE user_id = ? AND tweet_id = ?", userID, tweetID)
+	if err != nil {
+		log.Println(err)
+		return c.NoContent(500)
+	}
 
-// 	var likes []GetLikesResponse
-// 	err := h.db.Select(&likes, `
-// 		SELECT likes.id, tweets.*, users.display_id, users.name
-// 		FROM likes	
-// 		JOIN tweets ON likes.tweet_id = tweets.id
-// 		JOIN users ON tweets.user_id = users.id
-// 		WHERE likes.user_id = ?
-// 	`, userID)
-
-// 	if err != nil {
-// 		log.Println(err)
-// 		return c.JSON(500, map[string]string{"message": "Internal Server Error"})
-// 	}
-
-// 	return c.JSON(200, likes)
-// }
+	return c.NoContent(200)
+}
