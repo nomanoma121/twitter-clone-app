e7cc70a341776fdd0c81b2be74476cba99076bfe
kou <107801494+nomanoma121@users.noreply.github.com>
2025-01-24 19:18:08 +0900

    refactor: TTweetをインポートするようにした

--- client/src/hooks/use-tweets.ts
+++ client/src/hooks/use-tweets.ts
@@ -1,42 +1,9 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { serverFetch } from "../utils/fetch";
-
-type Tweet = {
-  id: number;
-  user: {
-    id: number;
-    display_id: string;
-    name: string;
-    icon_url: string;
-  };
-  content: string;
-  retweet: {
-    id: number;
-    user: {
-      id: number;
-      display_id: string;
-      name: string;
-      icon_url: string;
-    };
-    content: string | null;
-    interactions: {
-      retweet_count: number;
-      reply_count: number;
-      like_count: number;
-    };
-    created_at: string;
-  } | null;
-  interactions: {
-    retweet_count: number;
-    reply_count: number;
-    like_count: number;
-  };
-  liked_by_user: boolean;
-  created_at: string;
-};
+import { TTweet } from "../types/tweet";
 
 export const useTweets = (url: string) => {
-  const [tweets, setTweets] = useState<Tweet[]>([]);
+  const [tweets, setTweets] = useState<TTweet[]>([]);
 
   const fetchTweets = async () => {
     const res = await serverFetch(url);
