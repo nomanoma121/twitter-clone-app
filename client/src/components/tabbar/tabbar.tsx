0a7b176f6668a1e9ddf6fe26d1224d6132acea7b
kouta <takeuchi180121@gmail.com>
2025-01-28 14:33:22 +0900

    feat: タブバーのCSS実装完了

--- client/src/components/tabbar/tabbar.tsx
+++ client/src/components/tabbar/tabbar.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import "./tabbar.css";
 
 export const Tabbar = () => {
-  // タイムラインとフォロー中を切り替えるタブバー
+  const [isTimeline, setIsTimeline] = useState(true);
+
   return (
     <div className="Tabbar">
-      <div className="Tabbar__tab">タイムライン</div>
-      <div className="Tabbar__tab">フォロー中</div>
+      <div className="Tabbar__tab" onClick={() => setIsTimeline(true)}>
+        <div className={`Tabbar__tab__title ${isTimeline ? "active" : "" }`}>タイムライン</div>
+        <div className={`Tabbar__tab__border ${isTimeline ? "active" : "" }`} style={{ width: "34%" }} ></div>
+      </div>
+      <div className="Tabbar__tab" onClick={() => setIsTimeline(false)}>
+        <div className={`Tabbar__tab__title ${isTimeline ? "" : "active" }`}>フォロー中</div>
+        <div className={`Tabbar__tab__border ${isTimeline ? "" : "active" }`} style={{ width: "28%" }} ></div>
+      </div>
     </div>
- );
-}
+  );
+};
