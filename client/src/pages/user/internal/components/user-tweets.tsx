0532b7ca12b9c2a3108b8eb2e2bafd9687df4edf
kou <107801494+nomanoma121@users.noreply.github.com>
2025-01-28 18:55:38 +0900

    feat: ツイートと返信の切替タブを実装

--- client/src/pages/user/internal/components/user-tweets.tsx
+++ client/src/pages/user/internal/components/user-tweets.tsx
@@ -1,19 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { TweetList } from "../../../../components/tweet-list"
 import { useTweets } from "../../../../hooks/use-tweets";
 import { useLocation } from "react-router";
+import { Tabbar } from "../../../../components/tabbar";
 
 export const UserTweets = () => {
   const location = useLocation();
   const displayId = location.pathname.split("/")[1];
-  const { tweets, fetchTweets } = useTweets(`/api/users/${displayId}/tweets`);
+  const [endpoint, setEndpoint] = useState(`/api/users/${displayId}/tweets`);
+  const { tweets, fetchTweets } = useTweets(endpoint);
 
   useEffect(() => {
     fetchTweets();
-  }, [location]);
+  }, [location, endpoint]);
+
+  const switchTab = () => {
+    endpoint === `/api/users/${displayId}/tweets`
+      ? setEndpoint(`/api/users/${displayId}/replies`)
+      : setEndpoint(`/api/users/${displayId}/tweets`);
+  }
 
   return (
     <div>
+      <Tabbar titles={{first: "ツイート", second: "返信"}} switchTab={switchTab} />
       <TweetList tweets={tweets} refetch={fetchTweets} />
     </div>
   )
