427d8db3490bfeaa00ab48c23bd031199c172cb2
kouta <takeuchi180121@gmail.com>
2025-02-03 23:52:50 +0900

    refacter: フォーマット

--- server/model/tweet_image.go
+++ server/model/tweet_image.go
@@ -6,8 +6,8 @@ import (
 
 type TweetImage struct {
 	ID        int       `db:"id"`
-	TweetID    int       `db:"tweet_id"`
-	URL      string    `db:"url"`
+	TweetID   int       `db:"tweet_id"`
+	URL       string    `db:"url"`
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
 }
