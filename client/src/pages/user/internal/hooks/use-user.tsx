e931f9dee0ceb281f2f51ba78ea9beb694ecfce7
kouta <takeuchi180121@gmail.com>
2025-02-08 07:31:04 +0900

    feat: user型のschemaを作成

--- client/src/pages/user/internal/hooks/use-user.tsx
+++ client/src/pages/user/internal/hooks/use-user.tsx
@@ -1,22 +1,10 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { serverFetch } from "../../../../utils/fetch";
-
-type User = {
-  id: number;
-  display_id: string;
-  name: string;
-  icon_url: string;
-  header_url: string;
-  profile: string;
-  follower_counts: number;
-  followee_counts: number;
-  followed_by_user: boolean;
-  created_at: string;
-};
+import { TUser } from "../../../../types/user";
 
 export const useUser = (displayID: string) => {
-  const [userData, setUser] = useState<User | null>(null);
+  const [userData, setUser] = useState<TUser | null>(null);
 
   const fetchUser = async () => {
     const res = await serverFetch(`/api/users/${displayID}`);
---
+++ client/src/types/user.ts
@@ -0,0 +1,12 @@
+export type TUser = {
+  id: number;
+  display_id: string;
+  name: string;
+  icon_url: string;
+  header_url: string;
+  profile: string;
+  follower_counts: number;
+  followee_counts: number;
+  followed_by_user: boolean;
+  created_at: string;
+};
