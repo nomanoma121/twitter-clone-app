e3d62990837da69ba8c279d476491116eda2070e
kouta <takeuchi180121@gmail.com>
2025-02-08 16:19:01 +0900

    fix: 予期せずタブが切り替わってしまうバグを修正

--- client/src/components/tabbar/tabbar.tsx
+++ client/src/components/tabbar/tabbar.tsx
@@ -11,25 +11,40 @@ type TabbarProps = {
 };
 
 export const Tabbar = ({ titles, defaultTab, switchTab }: TabbarProps) => {
-  const [isTimeline, setIsTimeline] = useState(defaultTab == "second" ? false : true);
+  const [isTimeline, setIsTimeline] = useState(
+    defaultTab == "second" ? false : true
+  );
   const handleClick = (order: "first" | "second") => {
-    if (order === "first") {
+    if (order === "first" && !isTimeline) {
       setIsTimeline(true);
-    } else {
+    } else if (order === "second" && isTimeline) {
       setIsTimeline(false);
+    } else {
+      return;
     }
-  switchTab();
+
+    switchTab();
   };
 
   return (
     <div className="Tabbar">
       <div className="Tabbar__tab" onClick={() => handleClick("first")}>
-        <div className={`Tabbar__tab__title ${isTimeline ? "active" : "" }`}>{titles.first}</div>
-        <div className={`Tabbar__tab__border ${isTimeline ? "active" : "" }`} style={{ width: "34%" }} ></div>
+        <div className={`Tabbar__tab__title ${isTimeline ? "active" : ""}`}>
+          {titles.first}
+        </div>
+        <div
+          className={`Tabbar__tab__border ${isTimeline ? "active" : ""}`}
+          style={{ width: "34%" }}
+        ></div>
       </div>
       <div className="Tabbar__tab" onClick={() => handleClick("second")}>
-        <div className={`Tabbar__tab__title ${isTimeline ? "" : "active" }`}>{titles.second}</div>
-        <div className={`Tabbar__tab__border ${isTimeline ? "" : "active" }`} style={{ width: "28%" }} ></div>
+        <div className={`Tabbar__tab__title ${isTimeline ? "" : "active"}`}>
+          {titles.second}
+        </div>
+        <div
+          className={`Tabbar__tab__border ${isTimeline ? "" : "active"}`}
+          style={{ width: "28%" }}
+        ></div>
       </div>
     </div>
   );
